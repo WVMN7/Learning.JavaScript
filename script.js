128a54e6432efc9eb2c5d4be7e4a478b1c33fde5
WVMN <abbossabiryozov6@gmail.com>
2026-04-28 14:45:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// O'zgaruvchilar
+
+// 2 xil boladi
+
+let age = 22
+
+const name = 'Abbos'
+var fulName = 'Samar bad'
+
+
+{
+    var name =" Samar" // var bilan  {} tashqsqarida chaqirsa boladi 
+}
+
+console.log(name);
